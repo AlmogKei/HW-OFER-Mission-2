575823ebb82e059388b9c495ac826c9f48983906
AlmogKei <109092960+AlmogKei@users.noreply.github.com>
2024-02-07 23:44:05 +0200

    Revert "hw_ofer"
    This reverts commit 68ce8f7bff5f9e7c3197e029daeca4e03d5b4bac.

--- models/playerModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const playerSchema = new mongoose.Schema({
-    name:String,
-    age:Number
-})
-
-exports.playerSchema = mongoose.Mongoose.model("players",playerSchema)
